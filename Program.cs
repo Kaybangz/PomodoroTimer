6f96cf964e3fbb382c046733f4997dd09822d841
Kaybangz <igKaybangz@gmail.com>
2022-11-17 07:30:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+
+namespace Pomodoro_timer
+{
+    internal class Program
+    {
+        public static double ConvertMinutesToMilliseconds(int minutes)
+        {
+            return TimeSpan.FromMinutes(minutes).TotalMilliseconds;
+        }
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                //Prompt for the user to enter work and rest time
+                Console.WriteLine("Enter work time in minutes: ");
+                int workTime = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Work time saved, Great work buddy!");
+                
+
+                Console.WriteLine("\nEnter rest time in minutes: ");
+                int restTime = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Rest time saved...");
+
+
+                //Take user's input and convert to milliseconds using the ConvertToMilliseconds method
+                int convertWorkTimeToMs = (int)ConvertMinutesToMilliseconds(workTime);
+                int convertRestTimeToMs = (int)ConvertMinutesToMilliseconds(restTime);
+
+
+                //Create an instance of the PomoTimer class
+                PomoTimer timer = new PomoTimer(convertWorkTimeToMs, convertRestTimeToMs);
+
+
+
+                //Prompt for the user to start timer
+                Console.WriteLine("\nDo you want to start the timer(y/n): ");
+
+                string? startTimer = Console.ReadLine();
+
+
+                if(startTimer == "y")
+                {
+                    Console.WriteLine("\nWork timer is running...");
+                    timer.startWorkTime();
+
+                    Console.WriteLine("\nRest timer is running...");
+                    timer.startRestTime();
+                }
+
+                else if(startTimer == "n")
+                {
+                    continue;
+                }
+
+                else if(startTimer != "n" && startTimer != "y")
+               {
+                    Console.WriteLine("\nPlease enter a valid input, y for yes and n for no");
+                    continue;
+                }
+
+
+                //Prompt for user to continue using the timer
+                Console.WriteLine("\nWould you like to keep using the timer(y/n): ");
+
+                string? continueUsingTimer = Console.ReadLine();
+
+                if(continueUsingTimer == "y")
+                {
+                    continue;
+                }
+                else if(continueUsingTimer == "n")
+                {
+                    break;
+                }
+                else if (continueUsingTimer != "n" && continueUsingTimer != "y")
+                {
+                    Console.WriteLine("\nPlease enter a valid input next time, y for yes and n for no");
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Timers;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Pomodoro_timer
+{
+    public class PomoTimer
+    {
+
+        Stopwatch stopwatch = new Stopwatch();
+
+        private int workTime;
+        private int restTime;
+
+
+     
+
+        public PomoTimer(int workTime, int restTime)
+        {
+            this.workTime = workTime;
+            this.restTime = restTime;
+        }
+
+
+        public void startWorkTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            var workTimeStart = DateTime.Now.ToString("HH:mm");
+
+
+            Thread.Sleep(this.workTime);
+
+
+            stopWatch.Stop();
+            var workTimeEnd = DateTime.Now.ToString("HH:mm"); 
+
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+
+            Console.WriteLine($"\nWork time lasted for {elapsedTime} \n{workTimeStart} - {workTimeEnd}");
+        }
+
+
+
+        public void startRestTime()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            var restTimeStart = DateTime.Now.ToString("HH:mm");
+
+
+            Thread.Sleep(this.restTime);
+
+
+            stopWatch.Stop();
+            var restTimeEnd = DateTime.Now.ToString("HH:mm");
+
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+
+            Console.WriteLine($"\nRest time lasted for {elapsedTime} \n{restTimeStart} - {restTimeEnd}");
+        }
+
+
+    }
+}
